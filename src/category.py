e42fa55593ac5a2acce88e0fa38a64af834e68a1
Zver <andreevdamir266@gmail.com>
2024-09-19 22:54:43 +0300

    14.2

--- src/category.py
+++ src/category.py
@@ -7,12 +7,26 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
         Category.category_count += 1
-        Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    def products(self):
+        return self.__products
+
+    @property
+    def product_lists(self):
+        product_list = []
+        for product in self.__products:
+            product_info = f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+            product_list.append(product_info)
+        return '\n'.join(product_list)
 
 
 if __name__ == "__main__":
@@ -23,16 +37,20 @@ if __name__ == "__main__":
 
     category1 = Category(
         "Смартфоны",
-        "Смартфоны, как средство не только коммуникации," " но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     category2 = Category(
         "Телевизоры",
-        "Современный телевизор, " "который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-        [product4],
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
+
+    category1.add_product(product1)
+    category1.add_product(product2)
+    category1.add_product(product3)
+    category2.add_product(product4)
+
     print(category1.name)
     print(category1.description)
-    print(category1.products)
+    print(category1.product_lists)
     print(Category.category_count)
     print(Category.product_count)
